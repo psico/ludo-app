ef618ace11b8606c750f9bf21c0e55097238ba5c
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-17 20:37:43 -0300

    [UPT] Now using component footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./footer.css";
+import {
+    BottomNavigation,
+    BottomNavigationAction
+} from "@material-ui/core";
 
 const Footer = () => (
-    <div className="footer">
-        Footer here
-    </div>
+    <BottomNavigation
+        showLabels
+        className="footer"
+    >
+        <BottomNavigationAction label="Recents" value="recents"/>
+        <BottomNavigationAction label="Favorites" value="favorites"/>
+        <BottomNavigationAction label="Nearby" value="nearby" />
+        <BottomNavigationAction label="Folder" value="folder" />
+    </BottomNavigation>
 );
 
 export default Footer;

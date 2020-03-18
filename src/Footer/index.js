9c187b961c68b0b505c2be4e1346cc4c60a2185e
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-17 21:26:53 -0300

    [ADD] Using icons

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,20 +1,19 @@
 import React from "react";
 import "./footer.css";
-import {
-    BottomNavigation,
-    BottomNavigationAction
-} from "@material-ui/core";
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import { Home, Group, Dashboard, AccountCircle } from '@material-ui/icons';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 
 const Footer = () => (
     <BottomNavigation
-        showLabels
         className="footer"
     >
-        <BottomNavigationAction label="Community" value="community"/>
-        <BottomNavigationAction label="Friends" value="friends"/>
-        <BottomNavigationAction label="Add Match" value="add-match"/>
-        <BottomNavigationAction label="Games" value="games" />
-        <BottomNavigationAction label="My Profile" value="my-profile" />
+        <BottomNavigationAction label="Community" value="community" icon={<Home />}/>
+        <BottomNavigationAction label="Friends" value="friends" icon={<Group />}/>
+        <BottomNavigationAction label="Add Match" value="add-match" icon={<AddCircleOutlineIcon fontSize="large" color="action" />}/>
+        <BottomNavigationAction label="Dashboard" value="dashboard" icon={<Dashboard />}/>
+        <BottomNavigationAction label="My Profile" value="my-profile" icon={<AccountCircle />} />
     </BottomNavigation>
 );
 

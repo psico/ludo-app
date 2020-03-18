d0154f16bbfa712d29b0e69f8a7d2f84e658980c
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-17 21:28:05 -0300

    [UPT] Better code implements

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import "./footer.css";
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import { Home, Group, Dashboard, AccountCircle } from '@material-ui/icons';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import {Home, Group, Dashboard, AccountCircle, AddCircleOutline} from '@material-ui/icons';
 
 const Footer = () => (
     <BottomNavigation
         className="footer"
     >
-        <BottomNavigationAction label="Community" value="community" icon={<Home />}/>
-        <BottomNavigationAction label="Friends" value="friends" icon={<Group />}/>
-        <BottomNavigationAction label="Add Match" value="add-match" icon={<AddCircleOutlineIcon fontSize="large" color="action" />}/>
-        <BottomNavigationAction label="Dashboard" value="dashboard" icon={<Dashboard />}/>
-        <BottomNavigationAction label="My Profile" value="my-profile" icon={<AccountCircle />} />
+        <BottomNavigationAction label="Community" value="community" icon={<Home/>}/>
+        <BottomNavigationAction label="Friends" value="friends" icon={<Group/>}/>
+        <BottomNavigationAction label="Add Match" value="add-match"
+                                icon={<AddCircleOutline fontSize="large" color="action"/>}/>
+        <BottomNavigationAction label="Dashboard" value="dashboard" icon={<Dashboard/>}/>
+        <BottomNavigationAction label="My Profile" value="my-profile" icon={<AccountCircle/>}/>
     </BottomNavigation>
 );
 

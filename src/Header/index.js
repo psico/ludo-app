1cb99ebb4d6725bf76c881617478bccbfd79d087
João Gabriel dos Santos Rodrigues <mailparajg@gmail.com>
2020-03-18 09:41:48 -0300

    [UPT] Firts try to hide bar on scroll

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,21 +1,37 @@
 import React from "react";
 import "./header.css";
-import {AppBar, Toolbar} from '@material-ui/core';
+import {AppBar, Toolbar, useScrollTrigger, Slide} from '@material-ui/core';
 
-const Header = () => (
+function HideOnScroll(props) {
+    const { children, window } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
+
+const Header = (props) => (
     <div>
-        <LogoHeader/>
+        <LogoHeader {...props}/>
         <InfoHeader/>
     </div>
 );
 
-const LogoHeader = () => (
-    <AppBar position="static">
-        <Toolbar>
-            Logo Header<br/>
-            Logo Header
-        </Toolbar>
-    </AppBar>
+const LogoHeader = (props) => (
+    <HideOnScroll {...props}>
+        <AppBar position="static">
+            <Toolbar>
+                Logo Header<br/>
+                Logo Header
+            </Toolbar>
+        </AppBar>
+    </HideOnScroll>
 );
 
 const InfoHeader = () => (
@@ -23,5 +39,6 @@ const InfoHeader = () => (
         Info header
     </div>
 );
+
 
 export default Header;

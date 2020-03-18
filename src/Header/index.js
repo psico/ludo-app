99244dd7fcd24e2f38f7c674e4a6728a2f4f2aab
João Gabriel dos Santos Rodrigues <mailparajg@gmail.com>
2020-03-18 11:18:30 -0300

    [UPT] Now logoheader hidden is working

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./header.css";
 import {AppBar, Toolbar, useScrollTrigger, Slide} from '@material-ui/core';
 
-function HideOnScroll(props) {
-    const { children, window } = props;
+const HideOnScroll = (props) => {
+    const {children, window} = props;
     // Note that you normally won't need to set the window ref as useScrollTrigger
     // will default to window.
     // This is only being set here because the demo is in an iframe.
-    const trigger = useScrollTrigger({ target: window ? window() : undefined });
+    const trigger = useScrollTrigger({target: window ? window() : undefined});
 
     return (
         <Slide appear={false} direction="down" in={!trigger}>
@@ -25,7 +25,7 @@ const Header = (props) => (
 
 const LogoHeader = (props) => (
     <HideOnScroll {...props}>
-        <AppBar position="static">
+        <AppBar>
             <Toolbar>
                 Logo Header<br/>
                 Logo Header

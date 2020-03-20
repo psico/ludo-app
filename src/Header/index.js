fb5e9fdea0e9bd4b27b1ee35c69dde3501a6febd
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-20 18:02:05 -0300

    [UPT] Separing styledComponent.js

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./header.css";
+import useStyles from "./styledComponent";
 import {
     AppBar,
     Toolbar,
@@ -7,75 +8,12 @@ import {
     Slide,
     IconButton,
     Typography,
-    InputBase,
-    fade,
-    makeStyles
+    InputBase
 } from '@material-ui/core';
 import {Menu as MenuIcon, Search as SearchIcon} from '@material-ui/icons';
 import logo from './ludo192.png';
 
 console.log(logo);
-
-const useStyles = makeStyles(theme => ({
-    grow: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        display: 'none',
-        [theme.breakpoints.up('sm')]: {
-            display: 'block',
-        },
-    },
-    search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginRight: theme.spacing(2),
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-            marginLeft: theme.spacing(3),
-            width: 'auto',
-        },
-    },
-    searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    inputRoot: {
-        color: 'inherit',
-    },
-    inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
-    infoHeader: {
-        position: "sticky",
-        top: "64px",
-        margin: 0,
-        padding: 0,
-        height: "200px",
-        width: "100%",
-        background: "#a464ff"
-    }
-}));
 
 const HideOnScroll = (props) => {
     const {children, window} = props;
---
+++ src/Header/styledComponent.js
@@ -0,0 +1,67 @@
+import {
+    fade,
+    makeStyles
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+    infoHeader: {
+        position: "sticky",
+        top: "64px",
+        margin: 0,
+        padding: 0,
+        height: "200px",
+        width: "100%",
+        background: "#a464ff"
+    }
+}));
+
+export default useStyles;

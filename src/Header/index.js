bbff5f10522374a6e629087f88c7d2e0dabd5d30
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-20 18:50:10 -0300

    [UPT] better code.

--- src/Header/index.js
+++ src/Header/index.js
@@ -9,24 +9,10 @@ import {
     Typography,
     InputBase
 } from '@material-ui/core';
-import {Menu as MenuIcon, Search as SearchIcon} from '@material-ui/icons';
+import {Search as SearchIcon} from '@material-ui/icons';
 import logo from './ludo192.png';
 
 console.log(logo);
-
-const HideOnScroll = (props) => {
-    const {children, window} = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
-    const trigger = useScrollTrigger({target: window ? window() : undefined});
-
-    return (
-        <Slide appear={false} direction="down" in={!trigger}>
-            {children}
-        </Slide>
-    );
-};
 
 const Header = (props) => (
     <div>
@@ -83,5 +69,15 @@ const InfoHeader = () => {
     );
 };
 
+const HideOnScroll = (props) => {
+    const {children} = props;
+    const trigger = useScrollTrigger();
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+};
 
 export default Header;

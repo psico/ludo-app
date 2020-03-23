527cb10bd4a51b6884e9c77ce055ebcffb4c1b9e
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-22 23:07:04 -0300

    [ADD] added firts exemple to image.

--- src/Header/InfoHeader.js
+++ src/Header/InfoHeader.js
@@ -2,6 +2,7 @@ import React from "react";
 import Avatar from '@material-ui/core/Avatar';
 import useStyles from "./styledComponent";
 import jg from '../temp-images/tempImage.jpg';
+import lvl from "../temp-images/lvl-coronel.png";
 
 const InfoHeader = () => {
     const classes = useStyles();
@@ -9,6 +10,7 @@ const InfoHeader = () => {
     return (
         <div className={classes.infoHeader}>
             <Avatar variant="rounded" alt="João Gabriel" src={jg} className={classes.small} />
+            <img src={lvl} alt="user lvl" height="25"/>
         </div>
     );
 };

fda74f89ab0614c7a18befefbe4568891c38ca63
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-24 16:28:16 -0300

    [ADD] added firts exemple to level and description.

--- src/Header/InfoHeader.js
+++ src/Header/InfoHeader.js
@@ -11,6 +11,9 @@ const InfoHeader = () => {
         <div className={classes.infoHeader}>
             <Avatar variant="rounded" alt="João Gabriel" src={jg} className={classes.small} />
             <img src={lvl} alt="user lvl" height="25"/>
+            <h6>
+                <div>Lv 30 - Dice Explorer</div>
+            </h6>
         </div>
     );
 };

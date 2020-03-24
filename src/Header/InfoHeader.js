a0239ae0ca2d308b12bdaff175c494ad041a9070
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-24 17:05:46 -0300

    [ADD] added experience bar

--- src/Header/InfoHeader.js
+++ src/Header/InfoHeader.js
@@ -3,17 +3,38 @@ import Avatar from '@material-ui/core/Avatar';
 import useStyles from "./styledComponent";
 import jg from '../temp-images/tempImage.jpg';
 import lvl from "../temp-images/lvl-coronel.png";
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { lighten, withStyles } from '@material-ui/core/styles';
 
 const InfoHeader = () => {
     const classes = useStyles();
+    const [completed, setCompleted] = React.useState(30);
+
+    const BorderLinearProgress = withStyles({
+        root: {
+            height: 10,
+            borderRadius: 2,
+            backgroundColor: lighten('#ff6c5c', 0.5),
+        },
+        bar: {
+            borderRadius: 3,
+            backgroundColor: '#ff6c5c',
+        },
+    })(LinearProgress);
 
     return (
         <div className={classes.infoHeader}>
             <Avatar variant="rounded" alt="João Gabriel" src={jg} className={classes.small} />
             <img src={lvl} alt="user lvl" height="25"/>
-            <h6>
-                <div>Lv 30 - Dice Explorer</div>
-            </h6>
+            <h5>
+                <div>Level 30 - Dice Explorer</div>
+                <BorderLinearProgress
+                    className={classes.margin}
+                    variant="determinate"
+                    color="secondary"
+                    value={completed}
+                />
+            </h5>
         </div>
     );
 };

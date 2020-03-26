c56b372e67a1b5be7dd9796a484ac5fcc0610af1
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-26 19:09:32 -0300

    [UPT] Necessary to test

--- src/Community/index.js
+++ src/Community/index.js
@@ -40,8 +40,54 @@ const Community = ({classes}) => {
                     <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                         <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
                     </Grid>
-
-
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
                 </Grid>
             </div>
         );

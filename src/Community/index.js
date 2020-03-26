040250e6768166927f2b57d0d3489c1d32f7f3d5
João Gabriel <desenvolvedorjg@gmail.com>
2020-03-25 21:18:03 -0300

    [UPT] Begin to show container.

--- src/Community/index.js
+++ src/Community/index.js
@@ -1,96 +1,51 @@
 import React from "react";
 import "./community.css";
+import useStyles from "../css";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const Community = ({ classes }) => (
-    <div>
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-        Community here <br />
-    </div>
-);
+const Community = ({classes}) => {
+        const mainClasse = useStyles();
+
+        return (
+            <div className={mainClasse.root}>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Paper className={mainClasse.paper}>xs=12sadfsadfsafd</Paper>
+                    </Grid>
+
+
+                </Grid>
+            </div>
+        );
+    }
+;
 
 export default Community;

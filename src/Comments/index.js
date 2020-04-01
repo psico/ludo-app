ee01c313ba137aefd211d1003a8e569154f512a5
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-01 20:27:13 -0300

    [UPT] Started to implements comments

---
+++ src/Comments/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import useStyles from "./css";
+
+const Community = () => {
+    const componentClasses = useStyles();
+
+    return (
+        <div>
+            comments
+        </div>
+    )
+};
+
+export default Community;

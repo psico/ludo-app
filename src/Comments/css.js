5721cab6419b3ba7865143b5a5d555f12dee1f6a
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-02 10:06:13 -0300

    [UPT] Style to comments

--- src/Comments/css.js
+++ src/Comments/css.js
@@ -2,6 +2,19 @@ import {
     makeStyles
 } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({}));
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        margin: 0,
+        fontFamily: "sans-serif",
+        textAlign: 'left'
+    },
+    paper: {
+        padding: theme.spacing(1)
+    },
+    item: {
+        paddingBottom: "10px"
+    }
+}));
 
 export default useStyles;

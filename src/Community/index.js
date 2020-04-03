11e76d4d08074577aed7c6a09264ebeba9baaf8a
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-03 17:05:45 -0300

    [UPT] better responsive layout

--- src/Community/index.js
+++ src/Community/index.js
@@ -16,17 +16,19 @@ const Community = () => {
                     <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={componentClasses.item}>
                         <Paper className={componentClasses.paper}>
                             <Grid container spacing={0}>
-                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6} className={componentClasses.vl}>
+                                <Grid item xs={12} sm={12} md={6} lg={12} xl={12}>
                                     <UserAvatar showName={true}/>
                                     <hr />
+                                </Grid>
+                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                    <img src={zombicide} alt="logo ludoApp" height="300"/>
+                                </Grid>
+                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
                                     <Grid container spacing={0}>
                                         <Comments postId={10}/>
                                     </Grid>
                                     <hr />
                                         <Likes postId={10}/>
-                                </Grid>
-                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                                    <img src={zombicide} alt="logo ludoApp" height="300"/>
                                 </Grid>
                             </Grid>
                         </Paper>

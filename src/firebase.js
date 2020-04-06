6ca9e037abe94098ecc9ddece0963600997f9d10
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-06 14:05:02 -0300

    [ADD] Firebase config.

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCGb39lkfIWcaMIu9NBy8wpF57JUwnPs0g",
+    authDomain: "ludoapp-b612.firebaseapp.com",
+    databaseURL: "https://ludoapp-b612.firebaseio.com",
+    projectId: "ludoapp-b612",
+    storageBucket: "ludoapp-b612.appspot.com",
+    messagingSenderId: "723114001632",
+    appId: "1:723114001632:web:4737b4c1bca3d8c58dfc85",
+    measurementId: "G-WSMPGC1PCR"
+};

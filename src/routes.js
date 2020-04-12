7207afcba2845d5132f41e87cf695421085e000f
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-12 09:36:07 -0300

    [ADD] Login route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import React from "react";
+
+import Login from "./Login";
 import Join from "./Join";
 import Community from "./Community";
 
 const routes = [
+    { name: "Login", path: "/login", exact: true, main: () => <Login /> },
     { name: "Join", path: "/join", exact: true, main: () => <Join /> },
     { name: "Community", path: "/community", exact: true, main: () => <Community /> }
 ];

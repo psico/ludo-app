f78e69034243fa0b127a50897645c24b35c70922
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-14 15:37:39 -0300

    [DEL] Removed no public route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,11 @@ import React from "react";
 
 import Login from "./Login";
 import Join from "./Join";
-import Community from "./Community";
+
 
 const routes = [
     { name: "Login", path: "/login", exact: true, main: () => <Login /> },
-    { name: "Join", path: "/join", exact: true, main: () => <Join /> },
-    { name: "Community", path: "/community", exact: true, main: () => <Community /> }
+    { name: "Join", path: "/join", exact: true, main: () => <Join /> }
 ];
 
 export default routes;

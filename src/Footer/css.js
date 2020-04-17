8156f9f7a9cb18516fc46ebc115f17b24fafd70d
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-16 23:27:29 -0300

    [ADD] New CSS in correct format.

---
+++ src/Footer/css.js
@@ -0,0 +1,13 @@
+import {
+    makeStyles
+} from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        background: "#3f51b5"
+    }
+}));
+
+export default useStyles;

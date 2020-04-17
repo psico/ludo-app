36b70875416eada2893960b82fcd3e5e5a233528
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-16 23:28:11 -0300

    [UPT] Now using the new css format.

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,18 +1,24 @@
 import React from "react";
-import "./footer.css";
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import {Home, Group, Dashboard, AccountCircle, AddCircleOutline} from '@material-ui/icons';
+import useStyles from "./css";
 
-const Footer = () => (
-    <BottomNavigation>
-        <BottomNavigationAction label="Community" value="community" icon={<Home/>} />
-        <BottomNavigationAction label="Friends" value="friends" icon={<Group/>}/>
-        <BottomNavigationAction label="Add Match" value="add-match"
-                                icon={<AddCircleOutline fontSize="default"/>}/>
-        <BottomNavigationAction label="Dashboard" value="dashboard" icon={<Dashboard/>}/>
-        <BottomNavigationAction label="My Profile" value="my-profile" icon={<AccountCircle/>}/>
-    </BottomNavigation>
-);
+const Footer = () => {
+    const componentClasses = useStyles();
+
+    return (
+        <BottomNavigation classes={{
+            root: componentClasses.root
+        }}>
+            <BottomNavigationAction label="Community" value="community" icon={<Home/>}/>
+            <BottomNavigationAction label="Friends" value="friends" icon={<Group/>}/>
+            <BottomNavigationAction label="Add Match" value="add-match"
+                                    icon={<AddCircleOutline fontSize="default"/>}/>
+            <BottomNavigationAction label="Dashboard" value="dashboard" icon={<Dashboard/>}/>
+            <BottomNavigationAction label="My Profile" value="my-profile" icon={<AccountCircle/>}/>
+        </BottomNavigation>
+    )
+};
 
 export default Footer;

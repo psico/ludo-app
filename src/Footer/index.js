034051ed0f418ab27077f5a7de2d40bf189120e3
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-16 23:01:37 -0300

    [FIX] Removed div to correct warning

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -5,17 +5,13 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import {Home, Group, Dashboard, AccountCircle, AddCircleOutline} from '@material-ui/icons';
 
 const Footer = () => (
-    <BottomNavigation
-        className="footer"
-    >
-        <div>
-        <BottomNavigationAction label="Community" value="community" icon={<Home/>}/>
+    <BottomNavigation>
+        <BottomNavigationAction label="Community" value="community" icon={<Home/>} />
         <BottomNavigationAction label="Friends" value="friends" icon={<Group/>}/>
         <BottomNavigationAction label="Add Match" value="add-match"
                                 icon={<AddCircleOutline fontSize="default"/>}/>
         <BottomNavigationAction label="Dashboard" value="dashboard" icon={<Dashboard/>}/>
         <BottomNavigationAction label="My Profile" value="my-profile" icon={<AccountCircle/>}/>
-        </div>
     </BottomNavigation>
 );
 

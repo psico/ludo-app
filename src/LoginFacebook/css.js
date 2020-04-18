ec3718a90a1ec58377f90351131e0f37899ae458
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-18 12:06:17 -0300

    [ADD] Copy google button to make a facebook button

---
+++ src/LoginFacebook/css.js
@@ -0,0 +1,11 @@
+import {
+    makeStyles
+} from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        background: "#ffffff",
+    }
+}));
+
+export default useStyles;
---
+++ src/LoginFacebook/index.js
@@ -0,0 +1,52 @@
+import React, {useState, useContext} from "react";
+import Button from '@material-ui/core/Button';
+import {AuthContext} from "../App";
+import firebase from "../firebase";
+import {withRouter} from 'react-router-dom'
+import useStyles from "./css";
+
+
+const LoginFacebook = ({history}) => {
+
+    const componentClasses = useStyles();
+
+    const Auth = useContext(AuthContext);
+
+    const [error, setErrors] = useState("");
+
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        firebase
+            .auth()
+            .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(() => {
+                firebase
+                    .auth()
+                    .signInWithPopup(provider)
+                    .then(result => {
+                        console.log(result);
+                        Auth.setLoggedIn(true);
+                        history.push('/community');
+                    })
+                    .catch(e => setErrors(e.message))
+            })
+
+    };
+
+    return (
+        <Button onClick={() => signInWithGoogle()}
+                variant="contained" type="button"
+                classes={{
+                    root: componentClasses.root
+                }}>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="logo"
+                height={20}
+            />&nbsp;Login With Google
+        </Button>
+
+    );
+};
+
+export default withRouter(LoginFacebook);

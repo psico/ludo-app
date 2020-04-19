43a2a66558f5c713316855d1ca74949a028fafd8
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-19 11:30:34 -0300

    [ADD] Copy facebook component to create a new component LoginTwitter

---
+++ src/LoginTwitter/css.js
@@ -0,0 +1,11 @@
+import {
+    makeStyles
+} from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    root: {
+        background: "#3b5998",
+    }
+}));
+
+export default useStyles;
---
+++ src/LoginTwitter/index.js
@@ -0,0 +1,52 @@
+import React, {useState, useContext} from "react";
+import Button from '@material-ui/core/Button';
+import {AuthContext} from "../App";
+import firebase from "../firebase";
+import {withRouter} from 'react-router-dom'
+import useStyles from "./css";
+
+
+const LoginFacebook = ({history}) => {
+
+    const componentClasses = useStyles();
+
+    const Auth = useContext(AuthContext);
+
+    const [error, setErrors] = useState("");
+
+    const signInWithFacebook = () => {
+        const provider = new firebase.auth.FacebookAuthProvider();
+        firebase
+            .auth()
+            .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            .then(() => {
+                firebase
+                    .auth()
+                    .signInWithPopup(provider)
+                    .then(result => {
+                        console.log(result);
+                        Auth.setLoggedIn(true);
+                        history.push('/community');
+                    })
+                    .catch(e => setErrors(e.message))
+            })
+
+    };
+
+    return (
+        <Button onClick={() => signInWithFacebook()}
+                variant="contained" type="button"
+                classes={{
+                    root: componentClasses.root
+                }}>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/f/fb/Facebook_icon_2013.svg"
+                alt="logo"
+                height={20}
+            />&nbsp;Login With Facebook
+        </Button>
+
+    );
+};
+
+export default withRouter(LoginFacebook);

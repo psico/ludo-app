9e4e451cfadfc48780c4e2c30405e20797c0231f
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-23 15:37:59 -0300

    [ADD] install i18next

--- src/Header/InfoHeader.js
+++ src/Header/InfoHeader.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import componentStyles from "./css";
 import lvl from "../temp-images/lvl-coronel.png";
 import LinearProgress from '@material-ui/core/LinearProgress';
 import { lighten, withStyles } from '@material-ui/core/styles';
 import UserAvatar from "../UserAvatar";
+import { AuthContext } from "../App";
 
 const InfoHeader = () => {
     const classes = componentStyles();
     const [completed] = React.useState(30);
+
+    const {isLoggedIn} = useContext(AuthContext);
 
     const BorderLinearProgress = withStyles({
         root: {
@@ -21,12 +24,14 @@ const InfoHeader = () => {
         },
     })(LinearProgress);
 
+    console.log('aaaa- ',isLoggedIn);
+
     return (
         <div className={classes.infoHeader}>
             <UserAvatar showName={false} />
             <img src={lvl} alt="user lvl" height="25"/>
             <h5>
-                <div>Level 30 - Dice Explorer</div>
+                <div>Level 30 - Dice Explorer {isLoggedIn}</div>
                 <BorderLinearProgress
                     className={classes.margin}
                     variant="determinate"

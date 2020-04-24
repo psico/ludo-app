3617b6987fb1f75dc8dc03251879f6473c07c540
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-24 20:06:05 -0300

    [UPT] Footer items only show if is logged

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, {useContext} from "react";
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import {Home, Group, Dashboard, AccountCircle, AddCircleOutline} from '@material-ui/icons';
 import useStyles from "./css";
+import {AuthContext} from "../App";
 
 const Footer = () => {
     const componentClasses = useStyles();
+    const {isLoggedIn} = useContext(AuthContext);
 
     return (
-        <BottomNavigation classes={{
-            root: componentClasses.root
-        }}>
-            <BottomNavigationAction label="Community" value="community" icon={<Home/>}/>
-            <BottomNavigationAction label="Friends" value="friends" icon={<Group/>}/>
-            <BottomNavigationAction label="Add Match" value="add-match"
-                                    icon={<AddCircleOutline fontSize="default"/>}/>
-            <BottomNavigationAction label="Dashboard" value="dashboard" icon={<Dashboard/>}/>
-            <BottomNavigationAction label="My Profile" value="my-profile" icon={<AccountCircle/>}/>
-        </BottomNavigation>
+        <span>
+        {
+            isLoggedIn ?
+                <BottomNavigation classes={{root: componentClasses.root}}>
+                    <BottomNavigationAction label="Community" value="community" icon={<Home/>}/>
+                    <BottomNavigationAction label="Friends" value="friends" icon={<Group/>}/>
+                    <BottomNavigationAction label="Add Match" value="add-match"
+                                            icon={<AddCircleOutline fontSize="default"/>}/>
+                    <BottomNavigationAction label="Dashboard" value="dashboard" icon={<Dashboard/>}/>
+                    <BottomNavigationAction label="My Profile" value="my-profile" icon={<AccountCircle/>}/>
+
+                </BottomNavigation>
+                :
+                <BottomNavigation classes={{root: componentClasses.root}} />
+        }
+        </span>
     )
 };
 

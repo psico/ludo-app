bce551d4d0b2cf351a26be44d0ae5f9ce4d5eaf4
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-24 20:18:05 -0300

    [UPT] better code.

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -4,28 +4,27 @@ import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import {Home, Group, Dashboard, AccountCircle, AddCircleOutline} from '@material-ui/icons';
 import useStyles from "./css";
 import {AuthContext} from "../App";
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 const Footer = () => {
     const componentClasses = useStyles();
     const {isLoggedIn} = useContext(AuthContext);
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     return (
         <span>
-        {
-            isLoggedIn ?
-                <BottomNavigation classes={{root: componentClasses.root}}>
-                    <BottomNavigationAction label={t('community')} value="community" icon={<Home/>}/>
-                    <BottomNavigationAction label={t('friends')} value="friends" icon={<Group/>}/>
-                    <BottomNavigationAction label={t('add-match')} value="add-match"
-                                            icon={<AddCircleOutline fontSize="default"/>}/>
-                    <BottomNavigationAction label={t('dashboard')} value="dashboard" icon={<Dashboard/>}/>
-                    <BottomNavigationAction label={t('my-profile')} value="my-profile" icon={<AccountCircle/>}/>
+        {isLoggedIn ?
+            <BottomNavigation classes={{root: componentClasses.root}}>
+                <BottomNavigationAction label={t('community')} value="community" icon={<Home/>}/>
+                <BottomNavigationAction label={t('friends')} value="friends" icon={<Group/>}/>
+                <BottomNavigationAction label={t('add-match')} value="add-match"
+                                        icon={<AddCircleOutline fontSize="default"/>}/>
+                <BottomNavigationAction label={t('dashboard')} value="dashboard" icon={<Dashboard/>}/>
+                <BottomNavigationAction label={t('my-profile')} value="my-profile" icon={<AccountCircle/>}/>
 
-                </BottomNavigation>
-                :
-                <BottomNavigation classes={{root: componentClasses.root}} />
+            </BottomNavigation>
+            :
+            <BottomNavigation classes={{root: componentClasses.root}}/>
         }
         </span>
     )

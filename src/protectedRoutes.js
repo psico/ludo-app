aa46c818a3cd594df24a44438b3fffc32b655aaf
João Gabriel <desenvolvedorjg@gmail.com>
2020-04-26 13:36:27 -0300

    [FIX] Fix component to call

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Community from "./Community";
+import AddMatch from "./AddMatch";
 
 const protectedRoutes = [
     {name: 'Community', path: '/community', exact: true, main: props => <Community {...props} />, public: false},
-    {name: 'Add Match', path: '/addMatch', exact: true, main: props => <Community {...props} />, public: false},
+    {name: 'Add Match', path: '/addMatch', exact: true, main: props => <AddMatch {...props} />, public: false},
 ];
 
 export default protectedRoutes;

142b1953ac10745e6889d87dc961ff221fac2849
João Gabriel <desenvolvedorjg@gmail.com>
2020-05-01 22:32:41 -0300

    [ADD] Add new route game register.

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Community from "./Community";
 import AddMatch from "./AddMatch";
+import GameRegister from "./GameRegister";
 
 const protectedRoutes = [
     {name: 'Community', path: '/community', exact: true, main: props => <Community {...props} />, public: false},
     {name: 'Add Match', path: '/addMatch', exact: true, main: props => <AddMatch {...props} />, public: false},
+    {name: 'Game Register', path: '/gameRegister', exact: true, main: props => <GameRegister {...props} />, public: false}
 ];
 
 export default protectedRoutes;

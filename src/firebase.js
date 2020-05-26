55ff35a4db2e2ec470659b9c18e6d07e4e4ae3b1
João Gabriel <desenvolvedorjg@gmail.com>
2020-05-26 20:02:58 -0300

    [FIX] Resolving warning from firebase.

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
-import firebase from "firebase";
+// import firebase from "firebase";
+import firebase from "firebase/app";
+import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCGb39lkfIWcaMIu9NBy8wpF57JUwnPs0g",

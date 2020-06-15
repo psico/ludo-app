b6575fbb1f17108d03461f94b9ffe5c5a7a5301a
João Gabriel <desenvolvedorjg@gmail.com>
2020-06-14 23:29:43 -0300

    [ADD] Begin to implements context snackbar.

--- src/ShowSnackbar/index.js
+++ src/ShowSnackbar/index.js
@@ -6,35 +6,28 @@ function Alert(props) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
 
-export function handleClick(props) {
 
-// }
+const ShowSnackbar = (props) => {
+    // console.log(props);
+    // const [snackbar, setSnackbar] = React.useState({open: props.open, text: props.text});
+    //
+    // const showSnackbar = (text) => {
+    //     setSnackbar({...snackbar, open: true, text: text});
+    // };
+    //
+    // const closeSnackbar = (event, reason) => {
+    //     if (reason === 'clickaway') {
+    //         return;
+    //     }
+    //
+    //     setSnackbar({...snackbar, open: false});
+    // };
 
-    const ShowSnackbar = (props) => {
-        console.log(props);
-        const [snackbar, setSnackbar] = React.useState({open: props.open, text: props.text});
-
-        // const handleClick = (text) => {
-        //     setSnackbar({ ...snackbar, open: true, text: text });
-        // };
-
-        const handleClose = (event, reason) => {
-            if (reason === 'clickaway') {
-                return;
-            }
-
-            setSnackbar({...snackbar, open: false});
-        };
-
-        return (
-            <Snackbar open={snackbar.open} autoHideDuration={600} onClose={handleClose}>
-                <Alert onClose={handleClose} severity="success">
-                    {snackbar.text}
-                </Alert>
-            </Snackbar>
-        );
-    }
-
-    ShowSnackbar(props);
+    return (
+        <Snackbar open={snackbar.open} autoHideDuration={600} onClose={handleClose}>
+            <Alert onClose={handleClose} severity="success">
+                {snackbar.text}
+            </Alert>
+        </Snackbar>
+    );
 }
-

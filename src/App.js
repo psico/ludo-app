bd75c5dea3bcdd630f3de027a9b8cfef97f0d62e
João Gabriel <desenvolvedorjg@gmail.com>
2020-06-17 20:40:03 -0300

    [DEL] Removed snackbar to try another for

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import Header from "./Header";
 import Footer from "./Footer";
 import routes from "./routes";
 import protectedRoutes from "./protectedRoutes";
-import ShowSnackbar from "./ShowSnackbar";
 
 export const AuthContext = React.createContext({
     displayName: null,
@@ -19,83 +18,53 @@ export const AuthContext = React.createContext({
     isLoggedIn: false
 });
 
-export const SnackbarContext = React.createContext({
-    open: false,
-    text: 'Default Mensage',
-    severity: 'info',
-    showSnackbar: () => {
-    },
-    closeSnackbar: () => {
-    }
-})
-
 const App = () => {
     const [userInfo, setUserInfo] = useState(false);
-    const [snackbar, setSnackbar] = useState({open: false, text: 'Default Mensage'});
     const classes = useStyles();
-
-    const showSnackbar = (text) => {
-        setSnackbar({...snackbar, open: true, text: text});
-    };
-
-    const closeSnackbar = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setSnackbar({...snackbar, open: false});
-    };
 
     return (
         <AuthContext.Provider value={{userInfo, setUserInfo}}>
-            <SnackbarContext.Provider value={{snackbar, setSnackbar, showSnackbar, closeSnackbar}}>
-                <SnackbarContext.Consumer>
-                    {
-                        ({snackbar, setSnackbar, showSnackbar, closeSnackbar}) => (
-                        <BrowserRouter>
-                            <Grid container className={classes.root}>
-                                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                    <Header/>
-                                </Grid>
-                                <Grid container
-                                      direction="column"
-                                      justify="center"
-                                      alignItems="center">
-                                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                        <Switch>
-                                            {protectedRoutes.map(route => (
-                                                <ProtectedRouteHoc
-                                                    key={route.path}
-                                                    isLoggedIn={userInfo.isLoggedIn}
-                                                    path={route.path}
-                                                    component={route.main}
-                                                    exact={route.exact}
-                                                    public={route.public}
-                                                    {...{snackbar, setSnackbar, showSnackbar, closeSnackbar}}
-                                                />
-                                            ))}
-                                            {routes.map(route => (
-                                                <Route
-                                                    key={route.path}
-                                                    path={route.path}
-                                                    exact={route.exact}
-                                                    component={route.main}
-                                                    {...{snackbar, setSnackbar, showSnackbar, closeSnackbar}}
-                                                />
-                                            ))}
-                                        </Switch>
-                                    </Grid>
-                                </Grid>
-                                <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={classes.footer}>
-                                    <Footer/>
-                                </Grid>
-                                <ShowSnackbar {...{snackbar, setSnackbar}} />
-                            </Grid>
-                        </BrowserRouter>
-                        )
-                    }
-                </SnackbarContext.Consumer>
-            </SnackbarContext.Provider>
+            <BrowserRouter>
+                <Grid container className={classes.root}>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                        <Header/>
+                    </Grid>
+                    <Grid container
+                          direction="column"
+                          justify="center"
+                          alignItems="center">
+                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                            <Switch>
+                                {protectedRoutes.map(route => (
+                                    <ProtectedRouteHoc
+                                        key={route.path}
+                                        isLoggedIn={userInfo.isLoggedIn}
+                                        path={route.path}
+                                        component={route.main}
+                                        exact={route.exact}
+                                        public={route.public}
+                                        {...{snackbar, setSnackbar, showSnackbar, closeSnackbar}}
+                                    />
+                                ))}
+                                {routes.map(route => (
+                                    <Route
+                                        key={route.path}
+                                        path={route.path}
+                                        exact={route.exact}
+                                        component={route.main}
+                                        {...{snackbar, setSnackbar, showSnackbar, closeSnackbar}}
+                                    />
+                                ))}
+                            </Switch>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={classes.footer}>
+                        <Footer/>
+                    </Grid>
+                    <ShowSnackbar {...{snackbar, setSnackbar}} />
+                </Grid>
+            </BrowserRouter>
+
         </AuthContext.Provider>
     );
 };

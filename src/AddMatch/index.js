889118ce5777a281e10c4c9f2f0c954cc0d10211
João Gabriel <desenvolvedorjg@gmail.com>
2020-06-22 23:09:54 -0300

    [DEL] Removed show snackbar to restart implementaion.

--- src/AddMatch/index.js
+++ src/AddMatch/index.js
@@ -5,7 +5,6 @@ import Button from '@material-ui/core/Button';
 import firebase from "../firebase";
 import {withRouter} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
-// import {handleClick} from '../ShowSnackbar';
 import Paper from "@material-ui/core/Paper";
 import UserAvatar from "../UserAvatar";
 import TextField from '@material-ui/core/TextField';

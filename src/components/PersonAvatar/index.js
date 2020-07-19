3092125fabdc8da7c5efe7c25394c3b1f0d80400
psico <desenvolvedorjg@gmail.com>
2020-07-19 14:20:41 -0300

    [UPT] improved layout to show first letter when it doesn't has image.

--- src/components/PersonAvatar/index.js
+++ src/components/PersonAvatar/index.js
@@ -12,7 +12,9 @@ const PersonAvatar = (props) => {
 
     return (
         <div className={classes.root}>
-            <Avatar variant="rounded" alt={displayName} src={photoURL} className={classes.small}/>
+            <Avatar variant="rounded" alt={displayName} src={photoURL} className={classes.small}>
+                {displayName.substr(0,1)}
+            </Avatar>
             {showName === true && <div className={classes.textAvatar}>{displayName}</div>}
         </div>
     );

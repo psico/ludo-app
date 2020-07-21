d17f3c87dab5dae315149045dcb14b32b4d26bd2
psico <desenvolvedorjg@gmail.com>
2020-07-21 18:40:40 -0300

    [UPT] Improving layout

--- src/page/Community/index.js
+++ src/page/Community/index.js
@@ -6,8 +6,8 @@ import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import ShareIcon from '@material-ui/icons/Share';
 import Paper from '@material-ui/core/Paper';
 import {useTranslation} from "react-i18next";
-import UserAvatar from "../../components/UserAvatar";
 import zombicide from "../../temp-images/zombicide.jpg";
+import UserAvatar from "../../components/UserAvatar";
 import Comments from "../../components/Comments";
 import PersonAvatar from "../../components/PersonAvatar";
 import CommentInput from "../../components/CommentInput";
@@ -47,25 +47,25 @@ const Community = () => {
                               spacing={0}>
                             <Paper className={componentClasses.paper}>
                                 <Grid container spacing={0}>
-                                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                        {value.game ? value.game.name : ''}
-                                        <Grid container spacing={0}>
-                                            <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                                                <UserAvatar showName={true}/>
-                                                {/*<PersonAvatar displayName={value.uid} showName={true}/>*/}
-                                            </Grid>
-                                            <Grid item xs={12} sm={12} md={6} lg={6} xl={6} container spacing={0}>
-                                                {
-                                                    value.players &&
-                                                    value.players.map((player, index) =>
-                                                        <PersonAvatar
-                                                            key={"person_" + index}
-                                                            displayName={player.name}
-                                                            showName={false}/>
-                                                    )
-                                                }
-                                            </Grid>
+                                    {value.game ? value.game.name : ''}
+                                    <Grid container spacing={0}>
+                                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                            <UserAvatar showName={true}/>
+                                            {/*<PersonAvatar displayName={value.uid} showName={true}/>*/}
                                         </Grid>
+                                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} container spacing={0}>
+                                            {
+                                                value.players &&
+                                                value.players.map((player, index) =>
+                                                    <PersonAvatar
+                                                        key={"person_" + index}
+                                                        displayName={player.name}
+                                                        showName={false}/>
+                                                )
+                                            }
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                                         <hr/>
                                     </Grid>
                                     <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
@@ -76,9 +76,14 @@ const Community = () => {
                                             <Comments postId={10}/>
                                         </Grid>
                                     </Grid>
+                                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                        <hr/>
+                                    </Grid>
                                     <Grid container spacing={0}>
-                                        <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                        <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
                                             <ShareIcon/>
+                                        </Grid>
+                                        <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
                                             <ThumbUpAltIcon/>
                                         </Grid>
                                         <Grid item xs={12} sm={12} md={6} lg={6} xl={6} container spacing={0}>

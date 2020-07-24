44a8cf0f87f326950c6c5e83bc3c1c9b482d49c3
psico <desenvolvedorjg@gmail.com>
2020-07-24 19:13:18 -0300

    [UPT] Using translation

--- src/components/CommentInput/index.js
+++ src/components/CommentInput/index.js
@@ -7,14 +7,15 @@ import MessageIcon from '@material-ui/icons/Message';
 const CommentInput = (props) => {
     const componentClasses = useStyles();
     const {t} = useTranslation();
-    const [comment, setComment] = useState();
+    const [comment, setComment] = useState("");
 
     return (
         <div className={componentClasses.root}>
             <Input
                 value={comment}
+                onChange={e => setComment(e.target.value)}
                 fullWidth="true"
-                placeholder="Add comment"
+                placeholder={t("write-a-comment")}
                 endAdornment={<MessageIcon position="end">Kg</MessageIcon>}
             />
         </div>

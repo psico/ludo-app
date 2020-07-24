4e3f4e8acfc9ab7129c28ca34c188c0c2b5c476d
psico <desenvolvedorjg@gmail.com>
2020-07-24 18:51:27 -0300

    [ADD] Added icon to input of comment.

--- src/components/CommentInput/index.js
+++ src/components/CommentInput/index.js
@@ -1,20 +1,32 @@
 import React, {useState} from "react";
 import useStyles from "./css";
 import {useTranslation} from "react-i18next";
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import Input from '@material-ui/core/Input';
+import MessageIcon from '@material-ui/icons/Message';
 
 const CommentInput = (props) => {
     const componentClasses = useStyles();
     const {t} = useTranslation();
     const [comment, setComment] = useState();
 
+    // return (
+    //     <div className={componentClasses.root}>
+    //         <input
+    //             value={comment}
+    //             onChange={e => setComment(e.target.value)}
+    //             name="comment"
+    //             type="text"
+    //             placeholder={t('comment')}
+    //         />
+    //     </div>
+    // )
     return (
         <div className={componentClasses.root}>
-            <input
+            <Input
                 value={comment}
-                onChange={e => setComment(e.target.value)}
-                name="comment"
-                type="text"
-                placeholder={t('comment')}
+                endAdornment={<MessageIcon position="end">Kg</MessageIcon>}
             />
         </div>
     )

1754d4d65a0b52d8ae11da170b87b46765abd429
psico <desenvolvedorjg@gmail.com>
2020-07-25 22:25:18 -0300

    [ADD] Simple validation and snackbar warnning.

--- src/components/CommentInput/index.js
+++ src/components/CommentInput/index.js
@@ -3,19 +3,40 @@ import useStyles from "./css";
 import {useTranslation} from "react-i18next";
 import Input from '@material-ui/core/Input';
 import MessageIcon from '@material-ui/icons/Message';
+// import ShowSnackbar from "../../components/ShowSnackbar";
+import firebase from "../../firebase";
+import ShowSnackbar from "../ShowSnackbar";
 
 const CommentInput = (props) => {
     const componentClasses = useStyles();
     const {t} = useTranslation();
     const [comment, setComment] = useState("");
+    const snackbarRef = React.createRef();
 
     const handleForm = async e => {
         e.preventDefault();
         console.log(comment);
+        if (comment !== "") {
+            // let matches = await firebase.firestore().collection("matches").doc();
+            //
+            // match.uid = userInfo.uid;
+            //
+            // setMatch({
+            //     ...match
+            // })
+            //
+            // await matches.set(match, {merge: true});
+            //
+            // // snackbarRef.current.handleClick(t('successfully-registered-match'), 'success');
+            // history.push('/community');
+        } else {
+            snackbarRef.current.handleClick("test", 'warning');
+        }
     }
 
     return (
         <div className={componentClasses.root}>
+            <ShowSnackbar ref={snackbarRef}/>
             <form noValidate onSubmit={e => handleForm(e)}>
                 <Input
                     value={comment}

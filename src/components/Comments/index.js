c8b4c64a8e1e875e57aa3f258dd7bfd67ae2d4f7
psico <desenvolvedorjg@gmail.com>
2020-07-29 11:25:29 -0300

    [UPT] Using i18n

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -2,31 +2,31 @@ import React from "react";
 import useStyles from "./css";
 import Grid from "@material-ui/core/Grid";
 import PersonAvatar from "../PersonAvatar";
+import {useTranslation} from "react-i18next";
 
 const Comments = (props) => {
     const componentClasses = useStyles();
+    const {t} = useTranslation();
 
     return (
         <div className={componentClasses.root}>
             <Grid container spacing={0}>
                 {
-                    props.arrComments &&
-                    props.arrComments.map((comment, index) =>
+                    props.arrComments ?
+                        props.arrComments.map((comment, index) =>
+                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={componentClasses.item}>
+                                <PersonAvatar
+                                    key={"person_comment_" + index}
+                                    displayName={comment.name}
+                                    showName={true}/>
+                                {comment.comment}
+                            </Grid>
+                        )
+                        :
                         <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={componentClasses.item}>
-                            <PersonAvatar
-                                key={"person_comment_" + index}
-                                displayName={comment.name}
-                                showName={true}/>
-                            {comment.comment}
+                            {t("no-comments-made-you-can-be-the-first")}
                         </Grid>
-                    )
                 }
-                {/*<Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={componentClasses.item}>*/}
-                {/*    <UserAvatar showImage={true} showName={true}/>O bacana é que tem muitas expansões*/}
-                {/*</Grid>*/}
-                {/*<Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={componentClasses.item}>*/}
-                {/*    <UserAvatar showImage={true} showName={true}/>A expansão do Black Plague demoro mas chegou*/}
-                {/*</Grid>*/}
             </Grid>
         </div>
     )

4183e16aa89bd3b81c4c800d854493a1c19cdc98
psico <desenvolvedorjg@gmail.com>
2020-11-11 16:47:06 -0300

    [UPT] trying to use one connection

--- src/index.js
+++ src/index.js
@@ -4,10 +4,46 @@ import './index.css';
 import './i18n';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
+// import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const {REACT_APP_API_URL} = process.env;
+//Apollo requeriments
+
+const cache = new InMemoryCache();
+const GITHUB_BASE_URL = `${REACT_APP_API_URL}/graphql`
+
+// const httpLink = new HttpLink({
+//     uri: GITHUB_BASE_URL,
+//     headers: {
+//         authorization: `Bearer ${
+//             process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+//         }`,
+//     },
+// });
+const httpLink = new HttpLink({
+    uri: GITHUB_BASE_URL,
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache,
+});
+
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+// registerServiceWorker();

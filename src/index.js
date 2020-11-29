01d5046415c80615cbc1b74bd1ab83a0bc4b5072
psico <desenvolvedorjg@gmail.com>
2020-11-29 18:10:30 -0300

    [DEL] Rollback implemention.

--- src/index.js
+++ src/index.js
@@ -5,32 +5,7 @@ import './i18n';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
-// 1
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { createHttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-
-// 2
-const { REACT_APP_API_URL } = process.env;
-const httpLink = createHttpLink({
-    uri: `${REACT_APP_API_URL}/graphql`
-})
-
-// 3
-const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
-})
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-// 4
-ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

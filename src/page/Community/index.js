3d57b9bb2e5131407a8cd61528597495b22d0bb1
psico <desenvolvedorjg@gmail.com>
2021-04-09 17:29:05 -0300

    [UPT] removed unnecessary code

--- src/page/Community/index.js
+++ src/page/Community/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import useStyles from "./css";
 import Grid from '@material-ui/core/Grid';
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
@@ -32,77 +32,68 @@ const graphql = gql`
 `;
 
 function Community() {
-        const componentClasses = useStyles();
-        const {t} = useTranslation();
-        const { loading, error, data, refetch } = useQuery(graphql);
-        // const [communityList, setCommumnityList] = useState();
+    const componentClasses = useStyles();
+    const {t} = useTranslation();
+    const {data} = useQuery(graphql);
 
-        // useEffect(() => {
-        //     refetch();
-        //     if (loading) return ;
-        //     if (error) return ;
-        //     setCommumnityList(data.matches);
-        // }, [data, loading, error, setCommumnityList]);
-
-        return (
-            <div className={componentClasses.root}>
-                {data &&
-                data.matches.map((value, index) => {
-                    return (
-                        <Paper className={componentClasses.paper} key={"community_" + index}>
-                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}
-                                  className={componentClasses.item}
-                                  container
-                                  spacing={0}>
-                                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                                    {value.game ? value.game.name : ''}
+    return (
+        <div className={componentClasses.root}>
+            {data &&
+            data.matches.map((value, index) => {
+                return (
+                    <Paper className={componentClasses.paper} key={"community_" + index}>
+                        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}
+                              className={componentClasses.item}
+                              container
+                              spacing={0}>
+                            <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                                {value.game ? value.game.name : ''}
+                            </Grid>
+                            <Grid container className={componentClasses.hLine}>
+                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                    <UserAvatar showName={true}/>
+                                    {/*<PersonAvatar displayName={value.uid} showName={true}/>*/}
                                 </Grid>
-                                <Grid container className={componentClasses.hLine}>
-                                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                                        <UserAvatar showName={true}/>
-                                        {/*<PersonAvatar displayName={value.uid} showName={true}/>*/}
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6} container>
-                                        {
-                                            value.players &&
-                                            value.players.map((player, index) =>
-                                                <PersonAvatar
-                                                    key={"person_" + index}
-                                                    displayName={player.name}
-                                                    showName={false}/>
-                                            )
-                                        }
-                                    </Grid>
+                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6} container>
+                                    {
+                                        value.players &&
+                                        value.players.map((player, index) =>
+                                            <PersonAvatar
+                                                key={"person_" + index}
+                                                displayName={player.name}
+                                                showName={false}/>
+                                        )
+                                    }
                                 </Grid>
-                                <Grid container className={componentClasses.hLine}>
-                                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                                        <img src={zombicide} alt={t('logo-ludoapp')} height="300"/>
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                                        <Grid container spacing={0} item xs={12} sm={12} md={6} lg={6} xl={6}>
-                                            <Comments arrComments={value.comments}/>
-                                        </Grid>
-                                    </Grid>
+                            </Grid>
+                            <Grid container className={componentClasses.hLine}>
+                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                    <img src={zombicide} alt={t('logo-ludoapp')} height="300"/>
                                 </Grid>
-                                <Grid container>
-                                    <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
-                                        <ThumbUpAltIcon/> Like
-                                    </Grid>
-                                    <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
-                                        <ShareIcon/> Share
-                                    </Grid>
-                                    <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                                        <CommentInput matchId={value.idDoc}/>
+                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                    <Grid container spacing={0} item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                        <Comments arrComments={value.comments}/>
                                     </Grid>
                                 </Grid>
                             </Grid>
-                        </Paper>
-                    )
-                })
-                }
-            </div>
-        );
-    }
-;
+                            <Grid container>
+                                <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
+                                    <ThumbUpAltIcon/> Like
+                                </Grid>
+                                <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
+                                    <ShareIcon/> Share
+                                </Grid>
+                                <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+                                    <CommentInput matchId={value.idDoc}/>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Paper>
+                )
+            })
+            }
+        </div>
+    );
+}
 
 export default Community;

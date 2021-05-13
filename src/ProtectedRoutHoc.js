877a974b800d4c677709f13ca9f10241c2994989
psico <desenvolvedorjg@gmail.com>
2021-05-12 22:44:53 -0300

    [UPT] Testing request verifyToken

--- src/ProtectedRoutHoc.js
+++ src/ProtectedRoutHoc.js
@@ -3,6 +3,18 @@ import {Route, Redirect, withRouter} from 'react-router-dom';
 import {bool, any, object} from 'prop-types';
 
 const ProtectedRouteHoc = ({component: Component, isLoggedIn, ...rest}) => {
+    const storage = JSON.parse(localStorage.getItem("userInfo"));
+
+    fetch('http://localhost:4000/verifyToken', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            idToken: storage.token
+        })
+    }).then(data => data.json())
+
     if (isLoggedIn || rest.public) {
         return (
             <Route

0a6321e8c2cc23a3940f51a8226a7dd36e8b8932
psico <desenvolvedorjg@gmail.com>
2021-05-23 21:47:05 -0300

    [UPT] Now it's working and with a better code.

--- src/components/LoginGoogle/index.js
+++ src/components/LoginGoogle/index.js
@@ -14,29 +14,15 @@ const LoginGoogle = ({history}) => {
 
     const [error, setErrors] = useState("");
     const Auth = useContext(AuthContext);
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
     const signInWithGoogle = () => {
         const provider = new firebase.auth.GoogleAuthProvider();
-        // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-        // // firebase.auth().languageCode = 'it';
-        // console.log("aki", provider);
+
         firebase.auth()
             .signInWithPopup(provider)
             .then((result) => {
-                /** @type {firebase.auth.OAuthCredential} */
-                let credential = result.credential;
-
-                console.log("credential ", result);
-
-                // This gives you a Google Access Token. You can use it to access the Google API.
-                // var token = credential.accessToken;
-                // The signed-in user info.
-                // var user = result.user;
-                // ...
-
-
-                loginCredential(result).then(async result => {
+                loginCredential(result.credential).then(async result => {
                     console.log("testing back result ", result.user);
                     if (result.user) {
                         Auth.setUserInfo({
@@ -48,68 +34,17 @@ const LoginGoogle = ({history}) => {
                             isLoggedIn: true,
                             token: result.user.token
                         });
-                        // localStorage.setItem("idToken", result.user.token);
-                        // setToken(result.user.token);
                         history.push('/community');
                     }
                 }).catch(e => {
                     setErrors(e.message);
                 });
-
-
-
-            }).catch((error) => {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            // The email of the user's account used.
-            var email = error.email;
-            // The firebase.auth.AuthCredential type that was used.
-            var credential = error.credential;
-            // ...
-        });
-
-
-
-        // firebase
-        //     .auth()
-        //     .setPersistence(firebase.auth.Auth.Persistence.SESSION)
-        //     .then(() => {
-        //         firebase
-        //             .auth()
-        //             .signInWithPopup(provider)
-        //             .then(result => {
-        //                 Auth.setUserInfo({
-        //                     displayName: result.user.displayName,
-        //                     email: result.user.email,
-        //                     emailVerified: result.user.emailVerified,
-        //                     uid: result.user.uid,
-        //                     photoURL: result.user.photoURL,
-        //                     isLoggedIn: true
-        //                 });
-        //                 history.push('/community');
-        //             })
-        //             .catch(e => setErrors(e.message))
-        //     })
-
-        // loginProvider(provider).then(async result => {
-        //     if (result.user) {
-        //         Auth.setUserInfo({
-        //             displayName: result.user.displayName ? result.user.displayName : result.user.email,
-        //             email: result.user.email,
-        //             emailVerified: result.user.emailVerified,
-        //             uid: result.user.uid,
-        //             photoURL: result.user.photoURL,
-        //             isLoggedIn: true,
-        //             token: result.user.token
-        //         });
-        //         // localStorage.setItem("idToken", result.user.token);
-        //         // setToken(result.user.token);
-        //         history.push('/community');
-        //     }
-        // }).catch(e => {
-        //     setErrors(e.message);
-        // });
+            })
+            .catch((error) => {
+                console.error(error.code);
+                console.error(error.message);
+                console.error(error.credential);
+            });
     };
 
     return (

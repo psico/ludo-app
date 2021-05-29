399233b92b394586eb5866f75918599de4d1ef54
psico <desenvolvedorjg@gmail.com>
2021-05-29 15:41:25 -0300

    [UPT] Starting new implement login with facebook

--- src/components/LoginFacebook/index.js
+++ src/components/LoginFacebook/index.js
@@ -5,6 +5,7 @@ import firebase from "../../firebase";
 import {withRouter} from 'react-router-dom'
 import useStyles from "./css";
 import {useTranslation} from "react-i18next";
+import {loginCredential} from "../../page/Login";
 
 
 const LoginFacebook = ({history}) => {
@@ -13,31 +14,43 @@ const LoginFacebook = ({history}) => {
 
     const [error, setErrors] = useState("");
     const Auth = useContext(AuthContext);
-    const { t } = useTranslation();
+    const {t} = useTranslation();
 
-    const signInWithFacebook = () => {
-        const provider = new firebase.auth.FacebookAuthProvider();
-        firebase
-            .auth()
-            .setPersistence(firebase.auth.Auth.Persistence.SESSION)
-            .then(() => {
-                firebase
-                    .auth()
-                    .signInWithPopup(provider)
-                    .then(result => {
-                        Auth.setUserInfo({
-                            displayName: result.user.displayName,
-                            email: result.user.email,
-                            emailVerified: result.user.emailVerified,
-                            uid: result.user.uid,
-                            photoURL: result.user.photoURL,
-                            isLoggedIn: true
-                        });
-                        history.push('/community');
-                    })
-                    .catch(e => setErrors(e.message))
-            })
+    const signInWithFacebook = async () => {
+        try {
+            const provider = new firebase.auth.FacebookAuthProvider();
+            const credential = await firebase.auth().signInWithPopup(provider);
+            const userInfo = await loginCredential(credential);
 
+            if (userInfo) {
+                Auth.setUserInfo(userInfo);
+                history.push('/community');
+            }
+            // firebase
+            //     .auth()
+            //     .setPersistence(firebase.auth.Auth.Persistence.SESSION)
+            //     .then(() => {
+            //         firebase
+            //             .auth()
+            //             .signInWithPopup(provider)
+            //             .then(result => {
+            //                 Auth.setUserInfo({
+            //                     displayName: result.user.displayName,
+            //                     email: result.user.email,
+            //                     emailVerified: result.user.emailVerified,
+            //                     uid: result.user.uid,
+            //                     photoURL: result.user.photoURL,
+            //                     isLoggedIn: true
+            //                 });
+            //                 history.push('/community');
+            //             })
+            //             .catch(e => setErrors(e.message))
+            //     })
+        } catch (error) {
+            console.error("Error on try loggin with Facebook, ", error);
+            setErrors(error);
+            history.push('/')
+        }
     };
 
     return (

5617fba8f12adc59768200318a0bc525a82506e9
psico <desenvolvedorjg@gmail.com>
2021-06-03 18:14:40 -0300

    [ADD] adding firebase api with env values

---
+++ src/firebase.js
@@ -0,0 +1,31 @@
+// import firebase from "firebase";
+import firebase from "firebase/app";
+import 'firebase/database';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const {
+    REACT_APP_API_URL,
+    FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN,
+    FIREBASE_DATABASE_URL,
+    FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID
+} = process.env;
+
+const firebaseConfig = {
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    databaseURL: FIREBASE_DATABASE_URL,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;

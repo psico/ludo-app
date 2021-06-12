9e0ac395f390e6b01aa8839c328e6cb0aa8d770c
psico <desenvolvedorjg@gmail.com>
2021-06-12 15:31:54 -0300

    [UPT] It is not longer necessary to send uid.

--- src/page/Community/index.js
+++ src/page/Community/index.js
@@ -37,7 +37,6 @@ function Community() {
     const {data, refetch} = useQuery(graphql);
 
     const refreshUseQuery = async () => {
-        console.log("hello test");
         await refetch();
     };
 

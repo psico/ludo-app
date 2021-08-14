c95ece469ea9f4be52e4d103963cb23e7fa68bc2
psico <desenvolvedorjg@gmail.com>
2021-08-14 18:28:29 -0300

    [ADD] Starting new page profile

---
+++ src/page/Profile/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import useStyles from "./css";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { withRouter } from 'react-router-dom/cjs/react-router-dom';
+
+const Profile = () => {
+  const componentClasses = useStyles();
+
+  return (
+    <div className={componentClasses.root}>
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={componentClasses.item}>
+          <Paper className={componentClasses.paper}>
+            <h1>Hello profile</h1>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default withRouter(Profile);

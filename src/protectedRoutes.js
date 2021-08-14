f4041d2a04b5fbeb7f9741cb131e42dc593cd275
psico <desenvolvedorjg@gmail.com>
2021-08-14 18:29:18 -0300

    [UPT] Starting new page profile

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Community from "./page/Community";
 import AddMatch from "./page/AddMatch";
 import GameRegister from "./page/GameRegister";
+import Profile from './page/Profile';
 
 const protectedRoutes = [
     {name: 'Community', path: '/community', exact: true, main: props => <Community {...props} />, public: false},
     {name: 'Add Match', path: '/addMatch', exact: true, main: props => <AddMatch {...props} />, public: false},
-    {name: 'Game Register', path: '/gameRegister', exact: true, main: props => <GameRegister {...props} />, public: false}
+    {name: 'Game Register', path: '/gameRegister', exact: true, main: props => <GameRegister {...props} />, public: false},
+    {name: 'Profile', path: '/profile', exact: true, main: props => <Profile {...props} />, public: false}
 ];
 
 export default protectedRoutes;

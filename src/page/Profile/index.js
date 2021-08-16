eb0f119e69d36b4f38cb0ed36b125590d06777e6
psico <desenvolvedorjg@gmail.com>
2021-08-16 14:35:36 -0300

    [UPT] Adjusting profile

--- src/page/Profile/index.js
+++ src/page/Profile/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import useStyles from "./css";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import React from 'react';
+import useStyles from './css';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom';
 
 const Profile = () => {
@@ -12,12 +12,16 @@ const Profile = () => {
       <Grid container spacing={0}>
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12} className={componentClasses.item}>
           <Paper className={componentClasses.paper}>
-            <h1>Hello profile</h1>
+            <Grid container spacing={0}>
+              <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <h1>Hello profile</h1>
+               </Grid>
+            </Grid>
           </Paper>
         </Grid>
       </Grid>
     </div>
   );
-}
+};
 
 export default withRouter(Profile);

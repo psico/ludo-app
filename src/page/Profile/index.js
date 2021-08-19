8e4647e3c4bad5a06bd35902faec2d41745095cc
psico <desenvolvedorjg@gmail.com>
2021-08-18 22:25:14 -0300

    [UPT] Starting to create profile screen

--- src/page/Profile/index.js
+++ src/page/Profile/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import useStyles from './css';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom';
-import UserAvatar from '../../components/UserAvatar';
+import { AuthContext } from '../../App';
+import Avatar from '@material-ui/core/Avatar';
+import { Button } from '@material-ui/core';
 
 const Profile = () => {
   const componentClasses = useStyles();
+  const {userInfo} = useContext(AuthContext);
 
   return (
     <div className={componentClasses.root}>
       <Paper className={componentClasses.paper}>
         <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
-          <UserAvatar showName={false}/>
+          <Avatar variant="circle" alt={userInfo.displayName} src={userInfo.photoURL} />
+          <text>{userInfo.displayName}</text>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+          <Button variant="contained" >Follow</Button>
         </Grid>
       </Paper>
     </div>

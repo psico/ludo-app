f4ebaf973da4588dbab01966b9bf80cb9279426d
psico <desenvolvedorjg@gmail.com>
2021-08-22 22:32:18 -0300

    [UPT] Space last matches

--- src/page/Profile/index.js
+++ src/page/Profile/index.js
@@ -74,6 +74,13 @@ const Profile = () => {
           </Grid>
         </Grid>
       </Paper>
+      <Paper className={componentClasses.paper}>
+        <Grid container>
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <h2>{t('last-matches')}</h2>
+          </Grid>
+        </Grid>
+      </Paper>
     </div>
   );
 };

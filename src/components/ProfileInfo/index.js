c29b6bded52545ee28630c3f8765cf39b8bbc77a
psico <desenvolvedorjg@gmail.com>
2021-08-24 13:40:45 -0300

    [ADD] Starting component ProfileInfo

---
+++ src/components/ProfileInfo/index.js
@@ -0,0 +1,8 @@
+import useStyles from './css';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom';
+
+const ProfileInfo = () => {
+  const componentClasses = useStyles();
+};
+
+export default withRouter(ProfileInfo);

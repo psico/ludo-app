48146affc64108e63627ec4cf4369363723fe3d6
psico <desenvolvedorjg@gmail.com>
2021-08-27 16:01:03 -0300

    [UPT] class to avatar

--- src/components/ProfileInfo/css.js
+++ src/components/ProfileInfo/css.js
@@ -3,19 +3,6 @@ import {
 } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    flexGrow: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    maxWidth: "921px",
-    margin: 0,
-    paddingTop: "115px",
-    paddingBottom: "50px",
-    fontFamily: "sans-serif"
-  },
   paper: {
     padding: theme.spacing(1),
     marginBottom: "15px",
@@ -23,10 +10,10 @@ const useStyles = makeStyles(theme => ({
     width: "921px"
   },
   item: {
-    textAlign: 'center',
-    justifyContent: "center",
+    display: "flex",
+    flexDirection: "column",
     alignItems: "center",
-    paddingBottom: '10px'
+    fontFamily: "sans-serif"
   },
   avatar: {
     width: theme.spacing(20),

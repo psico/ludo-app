79ae89ae0b7939dc59640b59f8bc10473dc47318
psico <desenvolvedorjg@gmail.com>
2021-09-22 09:32:37 -0300

    [UPT] Trying add Matches data

--- src/components/LastMatches/index.js
+++ src/components/LastMatches/index.js
@@ -4,15 +4,23 @@ import { useTranslation } from 'react-i18next';
 import { Paper } from '@material-ui/core';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom';
 
-const LastMatches = () => {
+const LastMatches = ({ matches }) => {
   const componentClasses = useStyles();
   const { t } = useTranslation();
+  console.log("matches ==> ", matches);
 
   return (
     <Paper className={componentClasses.paper}>
       <p>{t('last-matches')}</p>
+      {
+        matches ? matches.map((match) => {
+          return <div>{match.game.name}</div>
+        })
+          :
+          <div>{t("no-data")}</div>
+      }
     </Paper>
   );
-}
+};
 
 export default withRouter(LastMatches);

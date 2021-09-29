bc25d12c328d7a2654c6a9293542d9b181419d84
psico <desenvolvedorjg@gmail.com>
2021-09-29 19:21:46 -0300

    [UPT] Starting last matches list

--- src/components/LastMatches/index.js
+++ src/components/LastMatches/index.js
@@ -7,17 +7,20 @@ import { withRouter } from 'react-router-dom/cjs/react-router-dom';
 const LastMatches = ({ matches }) => {
   const componentClasses = useStyles();
   const { t } = useTranslation();
-  console.log("matches ==> ", matches);
+  console.log('matches ==> ', matches);
 
   return (
     <Paper className={componentClasses.paper}>
       <p>{t('last-matches')}</p>
       {
-        matches ? matches.map((match) => {
-          return <div>{match.game.name}</div>
-        })
+        matches ? matches.map((match) =>
+            <div>
+              <img src={match.game.imageUrl || 'https://s3-us-west-1.amazonaws.com/5cc.images/games/empty+box.jpg'}
+                   alt={match.game.name} height="100"/>
+            </div>
+          )
           :
-          <div>{t("no-data")}</div>
+          <div>{t('no-data')}</div>
       }
     </Paper>
   );

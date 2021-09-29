b625b55b953c91815787d160b8d84a9b1a0000ee
psico <desenvolvedorjg@gmail.com>
2021-09-29 19:25:41 -0300

    [UPT] Starting last matches list

--- src/components/LastMatches/index.js
+++ src/components/LastMatches/index.js
@@ -3,6 +3,7 @@ import useStyles from './css';
 import { useTranslation } from 'react-i18next';
 import { Paper } from '@material-ui/core';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom';
+import Grid from '@material-ui/core/Grid';
 
 const LastMatches = ({ matches }) => {
   const componentClasses = useStyles();
@@ -12,16 +13,18 @@ const LastMatches = ({ matches }) => {
   return (
     <Paper className={componentClasses.paper}>
       <p>{t('last-matches')}</p>
+      <Grid container>
       {
         matches ? matches.map((match) =>
-            <div>
+            <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
               <img src={match.game.imageUrl || 'https://s3-us-west-1.amazonaws.com/5cc.images/games/empty+box.jpg'}
                    alt={match.game.name} height="100"/>
-            </div>
+            </Grid>
           )
           :
           <div>{t('no-data')}</div>
       }
+      </Grid>
     </Paper>
   );
 };

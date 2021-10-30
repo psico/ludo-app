a5329157f47190788f98e151e7ff0019af1e5b25
psico <desenvolvedorjg@gmail.com>
2021-10-30 20:15:12 -0300

    [UPT] Begin to get params

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -8,7 +8,7 @@ const protectedRoutes = [
     {name: 'Community', path: '/community', exact: true, main: props => <Community {...props} />, public: false},
     {name: 'Add Match', path: '/addMatch', exact: true, main: props => <AddMatch {...props} />, public: false},
     {name: 'Game Register', path: '/gameRegister', exact: true, main: props => <GameRegister {...props} />, public: false},
-    {name: 'Profile', path: '/profile', main: props => <Profile {...props} />, public: false}
+    {name: 'Profile', path: '/profile/:id', main: props => <Profile {...props} />, public: false}
 ];
 
 export default protectedRoutes;

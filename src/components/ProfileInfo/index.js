bb9e975fe1ad7861ef923bdf77242a9d1bd99cd3
psico <desenvolvedorjg@gmail.com>
2021-12-11 21:38:57 -0300

    [UPT] Beginning to call toFollow backend function

--- src/components/ProfileInfo/index.js
+++ src/components/ProfileInfo/index.js
@@ -49,9 +49,13 @@ const ProfileInfo = ({
     return userInfo?.uid === userProfileInfoData?.uid;
   }
 
-  async function toFollow () {
+  async function toFollow (followUid) {
     alert('hello');
-    await follow({ followUid: "Pr5X0qk6DeYut8paQ8hQ5s7kb8F3" });
+    await follow({
+      variables: {
+        followUid: followUid
+      }
+    });
   }
 
   return (
@@ -62,7 +66,7 @@ const ProfileInfo = ({
           <Grid item xs={3} sm={3} md={3} lg={3} xl={3} className={componentClasses.item}>
             <Avatar variant="rounded" alt={userProfileInfoData?.name} src={photoURL} className={componentClasses.avatar}/>
             <div className={componentClasses.grider}>{userProfileInfoData?.name}</div>
-            <Button variant="contained" disabled={followCondicional()} onClick={toFollow}>{t('follow')}</Button>
+            <Button variant="contained" disabled={followCondicional()} onClick={toFollow(userProfileInfoData?.uid)}>{t('follow')}</Button>
           </Grid>
 
           <Grid item xs={9} sm={9} md={9} lg={9} xl={9}>

6b8a085b3d7435f588852eeae9bc3f1c76515d66
psico <desenvolvedorjg@gmail.com>
2022-01-05 22:57:32 -0300

    [UPT] Begin to create page search

---
+++ src/page/Search/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom';
+
+const Search = () => {
+  return <div>Search</div>
+}
+
+export default withRouter(Search);

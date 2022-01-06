515364dc52aba2deba83914c4fb30c5a30b333be
psico <desenvolvedorjg@gmail.com>
2022-01-05 23:03:58 -0300

    [ADD] Adding route search

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -3,12 +3,14 @@ import Community from "./page/Community";
 import AddMatch from "./page/AddMatch";
 import GameRegister from "./page/GameRegister";
 import Profile from "./page/Profile";
+import Search from './page/Search';
 
 const protectedRoutes = [
     {name: 'Community', path: '/community', exact: true, main: props => <Community {...props} />, public: false},
     {name: 'Add Match', path: '/addMatch', exact: true, main: props => <AddMatch {...props} />, public: false},
     {name: 'Game Register', path: '/gameRegister', exact: true, main: props => <GameRegister {...props} />, public: false},
-    {name: 'Profile', path: '/profile/:id', main: props => <Profile {...props} />, public: false}
+    {name: 'Profile', path: '/profile/:id', main: props => <Profile {...props} />, public: false},
+    {name: 'Search', path: '/search/:search', main: props => <Search {...props} />, public: false}
 ];
 
 export default protectedRoutes;

a800deeecacceb42257a97a4e5066a25309cb013
psico <desenvolvedorjg@gmail.com>
2022-01-07 11:19:54 -0300

    [ADD] Calling MatchesSearch

--- src/page/Search/index.js
+++ src/page/Search/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom';
 import useStyles from '../Profile/css';
+import MatchesSearch from '../../components/MatchesSearch';
 
 const Search = () => {
   const componentClasses = useStyles();
 
-  return <div className={componentClasses.root}>Search</div>
+  return (
+    <div className={componentClasses.root}>
+      <MatchesSearch />
+    </div>
+)
 }
 
 export default withRouter(Search);

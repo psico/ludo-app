f20e0ec60f1d8da311855dacc9a3031671e49751
psico <desenvolvedorjg@gmail.com>
2022-01-11 22:52:44 -0300

    [UPT] Stylize component MatchesSearch

--- src/components/MatchesSearch/index.js
+++ src/components/MatchesSearch/index.js
@@ -3,6 +3,7 @@ import useStyles from '../ProfileInfo/css';
 import Paper from '@material-ui/core/Paper';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import UserAvatar from '../UserAvatar';
 
 const MatchesSearch = () => {
   const componentClasses = useStyles();
@@ -12,9 +13,14 @@ const MatchesSearch = () => {
       <h2>Matches Search</h2>
       <Grid container>
         <Grid item xs={2} sm={2} md={2} lg={2} xl={2}>
-          <p>game</p>
+
           <img src="https://cdn.shopify.com/s/files/1/0513/4077/1515/products/catan-board-game.jpg?v=1609629082"
                alt="game" height="80"/>
+          <p>game</p>
+          <UserAvatar
+            photoURL="https://lh6.googleusercontent.com/-6tboY56uBbQ/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJNh23LmB2ElsqSlf7PDZ9kmTvaehQ/photo.jpg"
+            displayName="João Gabriel" showName={true}
+          />
         </Grid>
       </Grid>
     </Paper>

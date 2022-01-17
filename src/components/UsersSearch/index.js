378fc98bca32ad562342e0256b34f7676f7a565d
psico <desenvolvedorjg@gmail.com>
2022-01-16 23:31:16 -0300

    [UPT] Stylize component UsersSearch

--- src/components/UsersSearch/index.js
+++ src/components/UsersSearch/index.js
@@ -4,9 +4,27 @@ import Paper from '@material-ui/core/Paper';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import UserAvatar from '../UserAvatar';
+import lvl from '../../temp-images/lvl-coronel.png';
+import { lighten, withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import { useTranslation } from 'react-i18next';
 
 const UsersSearch = () => {
   const componentClasses = useStyles();
+  const {t} = useTranslation();
+  const [completed] = React.useState(30);
+
+  const BorderLinearProgress = withStyles({
+    root: {
+      height: 10,
+      borderRadius: 2,
+      backgroundColor: lighten('#ff6c5c', 0.5),
+    },
+    bar: {
+      borderRadius: 3,
+      backgroundColor: '#ff6c5c',
+    },
+  })(LinearProgress);
 
   return (
     <Paper className={componentClasses.paper}>
@@ -17,6 +35,17 @@ const UsersSearch = () => {
             photoURL="https://lh6.googleusercontent.com/-6tboY56uBbQ/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJNh23LmB2ElsqSlf7PDZ9kmTvaehQ/photo.jpg"
             displayName="João Gabriel" showName={false}
           />
+          <p>João Gabriel</p>
+          <img src={lvl} alt={t('user-level')} height="25"/>
+          <h5>
+            <div>Level 30 - Dice Explorer {/** userInfo.isLoggedIn**/}</div>
+            <BorderLinearProgress
+              className={componentClasses.margin}
+              variant="determinate"
+              color="secondary"
+              value={completed}
+            />
+          </h5>
         </Grid>
       </Grid>
     </Paper>

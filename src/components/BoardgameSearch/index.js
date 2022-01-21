4498ce653e868844be1af8e8ef4d362cdf65e903
psico <desenvolvedorjg@gmail.com>
2022-01-20 21:02:22 -0300

    [UPT] Begin components BoardgameSearch

---
+++ src/components/BoardgameSearch/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom/cjs/react-router-dom';
+
+const BoardgameSearch = () => {
+  return (
+    <div> Boardgame Search </div>
+  )
+}
+export default withRouter(BoardgameSearch);

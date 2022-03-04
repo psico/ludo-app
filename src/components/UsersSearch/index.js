0156aa30988a68c7d2bcdf311c9686eb892aebe7
psico <desenvolvedorjg@gmail.com>
2022-03-04 16:41:28 -0300

    [UPT] This code will be implement

--- src/components/UsersSearch/index.js
+++ src/components/UsersSearch/index.js
@@ -4,9 +4,9 @@ import Paper from '@material-ui/core/Paper';
 import { withRouter } from 'react-router-dom/cjs/react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import UserAvatar from '../UserAvatar';
-import lvl from '../../temp-images/lvl-coronel.png';
-import { lighten, withStyles } from '@material-ui/core/styles';
-import LinearProgress from '@material-ui/core/LinearProgress';
+// import lvl from '../../temp-images/lvl-coronel.png';
+// import { lighten, withStyles } from '@material-ui/core/styles';
+// import LinearProgress from '@material-ui/core/LinearProgress';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
@@ -37,31 +37,30 @@ query usersInfo($textSearch: String) {
 const UsersSearch = () => {
   const componentClasses = useStyles();
   const { t } = useTranslation();
-  const [completed] = React.useState(30);
   const {
     data
   } = useQuery(graphql, {
     variables: { textSearch: useParams().search }
   });
 
-  const BorderLinearProgress = withStyles({
-    root: {
-      height: 10,
-      borderRadius: 2,
-      backgroundColor: lighten('#ff6c5c', 0.5),
-    },
-    bar: {
-      borderRadius: 3,
-      backgroundColor: '#ff6c5c',
-    },
-  })(LinearProgress);
+  // const BorderLinearProgress = withStyles({
+  //   root: {
+  //     height: 10,
+  //     borderRadius: 2,
+  //     backgroundColor: lighten('#ff6c5c', 0.5),
+  //   },
+  //   bar: {
+  //     borderRadius: 3,
+  //     backgroundColor: '#ff6c5c',
+  //   },
+  // })(LinearProgress);
 
   return (
     <Paper className={componentClasses.paper}>
       <h2>Users Search</h2>
 
       <Grid container>
-        {data?.usersInfo && data.usersInfo.map(userInfo =>
+        {data?.usersInfo?.length > 0 ? data.usersInfo.map(userInfo =>
             <span>
         <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
           <UserAvatar
@@ -83,7 +82,12 @@ const UsersSearch = () => {
               {/*    </span>*/}
               {/*</Grid>*/}
             </span>
-        )}
+        )
+        :
+          <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <p>{t('no-results')}</p>
+          </Grid>
+        }
       </Grid>
     </Paper>
   );

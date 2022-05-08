e38672516dea999cd9c5062d772fd8a979ea2e66
psico <desenvolvedorjg@gmail.com>
2022-05-07 22:20:48 -0300

    [UPT] Getting useExperienceInfo

--- src/components/Header/InfoHeader.js
+++ src/components/Header/InfoHeader.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext, useEffect } from 'react';
 import componentStyles from "./css";
 import lvl from "../../temp-images/lvl-coronel.png";
 import LinearProgress from '@material-ui/core/LinearProgress';
@@ -6,13 +6,31 @@ import {lighten, withStyles} from '@material-ui/core/styles';
 import UserAvatar from "../UserAvatar";
 import {AuthContext} from "../../App";
 import {useTranslation} from "react-i18next";
+import { gql, useQuery } from '@apollo/client';
+
+const graphql = gql`
+    query userExperienceInfo($uid: uid){
+        userExperienceInfo(uid: $uid) {
+            totalExperience
+            level
+            nextLevelExperience
+        }
+    }
+`;
 
 const InfoHeader = () => {
     const classes = componentStyles();
     const {t} = useTranslation();
     const [completed] = React.useState(30);
-
     const {userInfo} = useContext(AuthContext);
+    const {data} = useQuery(graphql, {
+        variables: {
+            "uid": "0IhNFZFa7QMwBY6yZT8l24L1AX32",
+        }
+    });
+
+    useEffect(() => {
+    }, [data]);
 
     const BorderLinearProgress = withStyles({
         root: {
@@ -36,7 +54,7 @@ const InfoHeader = () => {
                   showName={false}/>
                 <img src={lvl} alt={t('user-level')} height="25"/>
                 <h5>
-                    <div>Level 30 - Dice Explorer {userInfo.isLoggedIn}</div>
+                    <div>Level {console.log("data => ", data)} - Dice Explorer {userInfo.isLoggedIn}</div>
                     <BorderLinearProgress
                         className={classes.margin}
                         variant="determinate"

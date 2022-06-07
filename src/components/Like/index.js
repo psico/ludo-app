3bfeb16ae178f04184e1dde21edfd86147b844a3
psico <desenvolvedorjg@gmail.com>
2022-06-06 21:58:29 -0300

    [UPT] Developing like component

--- src/components/Like/index.js
+++ src/components/Like/index.js
@@ -1,7 +1,9 @@
 import { withRouter } from 'react-router-dom';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import React from '@types/react';
 
 const Like = () => {
-
+return <div><ThumbUpAltIcon/> Like</div>
 }
 
 export default withRouter(Like);

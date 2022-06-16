e70a03bf73363ff2251aa4c72841e299704be77f
psico <desenvolvedorjg@gmail.com>
2022-06-15 22:48:48 -0300

    [UPT] Developing like component

--- src/components/Like/index.js
+++ src/components/Like/index.js
@@ -1,23 +1,20 @@
 import { withRouter } from 'react-router-dom';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
 import React from 'react';
-// import { useState } from '@types/react';
+import { useState } from '@types/react';
 
 const Like = () => {
-  // const [atLeastOneLike, setAtLeastOneLike] = useState(false);
+  const [atLeastOneLike] = useState(false);
   const likeAction = () => alert('like it');
 
-  // return <div onClick={() => likeAction()}>
-  //   {atLeastOneLike ?
-  //     <span><ThumbUpAltIcon/> Like</span> :
-  //     <span><ThumbUpOutlinedIcon/> Like</span>
-  //   }
-  //
-  //
-  // </div>;
-
   return <div onClick={() => likeAction()}>
-      <span><ThumbUpAltIcon/> Like</span>
+    {atLeastOneLike ?
+      <span><ThumbUpAltIcon/> Like</span> :
+      <span><ThumbUpOutlinedIcon/> Like</span>
+    }
+
+
   </div>;
 };
 

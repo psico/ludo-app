095979e5e88f4115019e04c35f0d4dcca7bf6f10
psico <desenvolvedorjg@gmail.com>
2022-06-27 20:50:06 -0300

    [UPT] Developing like component

--- src/components/Like/index.js
+++ src/components/Like/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
-import { gql } from '@apollo/client';
+import { gql, useMutation } from '@apollo/client';
 
 const graphql = gql`
     mutation likeIt {
@@ -15,6 +15,7 @@ const graphql = gql`
 
 const Like = () => {
   const [atLeastOneLike] = useState(false);
+  const [likeIt] = useMutation(graphql);
   const likeAction = () => alert('like it');
 
   return <div onClick={() => likeAction()}>

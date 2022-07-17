c823a0666684d7205326722410b762056cf4a2fb
psico <desenvolvedorjg@gmail.com>
2022-07-16 21:23:31 -0300

    [UPT] Developing unlike

--- src/components/ProfileInfo/index.js
+++ src/components/ProfileInfo/index.js
@@ -87,9 +87,9 @@ const ProfileInfo = ({
     setCompleted((100*data?.userExperienceInfo?.nextLevelExperience)/data?.userExperienceInfo?.totalExperience);
   });
 
-  useEffect(() => {
-    calculatePercentBar();
-  }, [calculatePercentBar]);
+  // useEffect(() => {
+  //   calculatePercentBar();
+  // },[calculatePercentBar,data]);
 
   return (
     <Paper className={componentClasses.paper}>

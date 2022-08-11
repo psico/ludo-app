0927b908b00f875a4ec2d2413392b07bc9c537d1
psico <desenvolvedorjg@gmail.com>
2022-08-10 21:24:55 -0300

    [UPT] Developing like and unlike

--- src/components/Like/index.js
+++ src/components/Like/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbUpOutlinedIcon from '@material-ui/icons/ThumbUpOutlined';
 import { gql, useMutation } from '@apollo/client';
 import { useTranslation } from "react-i18next";
+import { AuthContext } from '../../App';
 
 const graphql = gql`
     mutation likeIt($idDoc: String) {
@@ -19,6 +20,7 @@ const graphql = gql`
 
 const Like = ({ idDoc }) => {
   const {t} = useTranslation();
+  const { userInfo } = useContext(AuthContext);
   const [atLeastOneLike, setAtLeastOneLike] = useState(false);
   const [likeIt] = useMutation(graphql);
 
@@ -30,7 +32,7 @@ const Like = ({ idDoc }) => {
         'idDoc': idDoc,
       }
     }).then((likeData) => {
-      console.log("Here ============> ", likeData);
+      console.log("Here ============> ", userInfo.uid, likeData.likes.find((like) => like.uid === userInfo.uid));
     });
   }
 

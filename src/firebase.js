0ff23d2d511b3650988e1a8d0ff60eb33ec424ad
psico <desenvolvedorjg@gmail.com>
2022-08-24 23:23:01 -0300

    [UPT] Migrating firebase 9

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 // import firebase from "firebase";
-import firebase from "firebase/app";
+import firebase from "firebase/compat/app";
 import 'firebase/database';
 import 'firebase/auth';
 import 'firebase/firestore';

e6030fa61d3b4db14ce311f76fdc53a3e5d7292a
psico <desenvolvedorjg@gmail.com>
2022-08-31 17:05:52 -0300

    [ADD] Begining page show match

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -4,10 +4,12 @@ import AddMatch from "./page/AddMatch";
 import GameRegister from "./page/GameRegister";
 import Profile from "./page/Profile";
 import Search from './page/Search';
+import Match from './page/Match';
 
 const protectedRoutes = [
     {name: 'Community', path: '/community', exact: true, main: props => <Community {...props} />, public: false},
     {name: 'Add Match', path: '/addMatch', exact: true, main: props => <AddMatch {...props} />, public: false},
+    {name: 'Match', path: '/match', exact: true, main: props => <Match {...props} />, public: false},
     {name: 'Game Register', path: '/gameRegister', exact: true, main: props => <GameRegister {...props} />, public: false},
     {name: 'Profile', path: '/profile/:id', main: props => <Profile {...props} />, public: false},
     {name: 'Search', path: '/search/:search', main: props => <Search {...props} />, public: false}

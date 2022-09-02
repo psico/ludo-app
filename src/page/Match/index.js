095f577624f1f4fc314df0f905d3561189e9feaf
psico <desenvolvedorjg@gmail.com>
2022-09-01 21:16:26 -0300

    [UPT] Developing page show match

--- src/page/Match/index.js
+++ src/page/Match/index.js
@@ -7,16 +7,40 @@ import { gql, useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom/cjs/react-router-dom';
 
 const graphqlUserInfo = gql`
-  
+query getMatch($idDoc: ID!) {
+  match(idDoc: $idDoc) {
+    idDoc
+    matchOwner {
+      uid
+      name
+      photoURL
+    }
+    gameMoment
+    game {
+      name
+    }
+    comments {
+      uid
+      name
+      photoURL
+      comment
+    }
+    players {
+      uid
+      name
+      photoURL
+    }
+    
+  }
+}
 `;
 
 const Match = () => {
     const componentClasses = useStyles();
     const {t} = useTranslation();
-    const {userInfo} = useContext(AuthContext);
 
     const { data } = useQuery(graphqlUserInfo, {
-        variables: { uid: useParams().id }
+        variables: { idDoc: useParams().id }
     });
 
     return (

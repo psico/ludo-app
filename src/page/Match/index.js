84be004cd4a504122d495beaf94d9d0360e21283
psico <desenvolvedorjg@gmail.com>
2022-09-04 21:58:04 -0300

    [UPT] Developing page show match

--- src/page/Match/index.js
+++ src/page/Match/index.js
@@ -1,8 +1,7 @@
-import React, {useContext } from "react";
+import React from "react";
 import useStyles from "./css";
 import {withRouter} from 'react-router-dom';
 import {useTranslation} from 'react-i18next';
-import {AuthContext} from "../../App";
 import { gql, useQuery } from '@apollo/client';
 import { useParams } from 'react-router-dom/cjs/react-router-dom';
 import Paper from '@material-ui/core/Paper';

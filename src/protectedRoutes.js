7784935ce7f43df5f400cd2d29f989c1dddcd038
psico <desenvolvedorjg@gmail.com>
2022-09-11 18:13:50 -0300

    [UPT] Developing page show match

--- src/protectedRoutes.js
+++ src/protectedRoutes.js
@@ -4,13 +4,13 @@ import AddMatch from "./page/AddMatch";
 import GameRegister from "./page/GameRegister";
 import Profile from "./page/Profile";
 import Search from './page/Search';
-import Match from './page/Match';
+import Match from './page/ShowMatch';
 
 const protectedRoutes = [
     {name: 'Add Match', path: '/addMatch', exact: true, main: props => <AddMatch {...props} />, public: false},
     {name: 'Community', path: '/community', exact: true, main: props => <Community {...props} />, public: false},
     {name: 'Game Register', path: '/gameRegister', exact: true, main: props => <GameRegister {...props} />, public: false},
-    {name: 'Match', path: '/match/:id', exact: true, main: props => <Match {...props} />, public: false},
+    {name: 'Match', path: '/showMatch/:id', exact: true, main: props => <Match {...props} />, public: false},
     {name: 'Profile', path: '/profile/:id', main: props => <Profile {...props} />, public: false},
     {name: 'Search', path: '/search/:search', main: props => <Search {...props} />, public: false}
 ];

d320748f19d63afe2679473dec08ed4a5875fb37
psico <desenvolvedorjg@gmail.com>
2022-09-14 15:25:07 -0300

    [UPT] Developing new component match

--- src/components/Match/index.js
+++ src/components/Match/index.js
@@ -24,67 +24,67 @@ const Match = ({match, refreshUseQuery, history }) => {
 
   console.log('match 2 ==> ', match, refreshUseQuery, history);
 
-  // return (
-  //   <Paper className={componentClasses.paper} key={'community_' + match?.match?.matchOwner?.uid}>
-  //     <Grid item xs={12} sm={12} md={12} lg={12} xl={12}
-  //           className={componentClasses.item}
-  //           container
-  //           spacing={0}>
-  //       <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-  //         {match?.match?.game ? match?.match.game.name : ''}
-  //       </Grid>
-  //       <Grid container className={componentClasses.hLine}>
-  //         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-  //           <div onClick={() => {
-  //             history.push('/profile/' + match?.match?.matchOwner?.uid);
-  //           }}>
-  //             <UserAvatar photoURL={match?.match?.matchOwner?.photoURL} displayName={match?.match?.matchOwner?.name}
-  //                         showName={true}/>
-  //             {/*<PersonAvatar displayName={match?.match.uid} showName={true}/>*/}
-  //           </div>
-  //         </Grid>
-  //         <Grid item xs={12} sm={12} md={6} lg={6} xl={6} container>
-  //           {
-  //             match?.match.players &&
-  //             match?.match.players.map((player, index) =>
-  //               <PersonAvatar
-  //                 key={'person_' + index}
-  //                 displayName={player.name}
-  //                 showName={false}/>
-  //             )
-  //           }
-  //         </Grid>
-  //       </Grid>
-  //       <Grid container className={componentClasses.hLine}>
-  //         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-  //           <img src={match?.match.game.imageUrl || defaultImg} alt={t('logo-ludoapp')} height="300"/>
-  //         </Grid>
-  //         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-  //           <Grid container spacing={0} item xs={12} sm={12} md={6} lg={6} xl={6}>
-  //             <Comments arrComments={match?.match.comments}/>
-  //           </Grid>
-  //         </Grid>
-  //       </Grid>
-  //       <Grid container>
-  //         <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
-  //           <Like idDoc={useParams().id} likes={match?.match.likes}/>
-  //         </Grid>
-  //         <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
-  //           <ShareIcon onClick={() => {
-  //             navigator.share({
-  //               title: 'MDN',
-  //               text: 'Aprenda desenvolvimento web no MDN!',
-  //               url: 'https://developer.mozilla.org',
-  //             }).then(() => null);
-  //           }}/> Share
-  //         </Grid>
-  //         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-  //           <CommentInput matchId={useParams().id} refreshUseQuery={refreshUseQuery}/>
-  //         </Grid>
-  //       </Grid>
-  //     </Grid>
-  //   </Paper>
-  // );
+  return (
+    <Paper className={componentClasses.paper} key={'community_' + match?.match?.matchOwner?.uid}>
+      <Grid item xs={12} sm={12} md={12} lg={12} xl={12}
+            className={componentClasses.item}
+            container
+            spacing={0}>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+          {match?.match?.game ? match?.match.game.name : ''}
+        </Grid>
+        <Grid container className={componentClasses.hLine}>
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <div onClick={() => {
+              history.push('/profile/' + match?.match?.matchOwner?.uid);
+            }}>
+              <UserAvatar photoURL={match?.match?.matchOwner?.photoURL} displayName={match?.match?.matchOwner?.name}
+                          showName={true}/>
+              {/*<PersonAvatar displayName={match?.match.uid} showName={true}/>*/}
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6} container>
+            {
+              match?.match.players &&
+              match?.match.players.map((player, index) =>
+                <PersonAvatar
+                  key={'person_' + index}
+                  displayName={player.name}
+                  showName={false}/>
+              )
+            }
+          </Grid>
+        </Grid>
+        <Grid container className={componentClasses.hLine}>
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <img src={match?.match.game.imageUrl || defaultImg} alt={t('logo-ludoapp')} height="300"/>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <Grid container spacing={0} item xs={12} sm={12} md={6} lg={6} xl={6}>
+              <Comments arrComments={match?.match.comments}/>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
+            <Like idDoc={useParams().id} likes={match?.match.likes}/>
+          </Grid>
+          <Grid item xs={6} sm={6} md={3} lg={3} xl={3}>
+            <ShareIcon onClick={() => {
+              navigator.share({
+                title: 'MDN',
+                text: 'Aprenda desenvolvimento web no MDN!',
+                url: 'https://developer.mozilla.org',
+              }).then(() => null);
+            }}/> Share
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+            <CommentInput matchId={useParams().id} refreshUseQuery={refreshUseQuery}/>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
 };
 
 export default withRouter(Match);

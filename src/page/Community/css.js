8ec3db461086f3e7e384eab8070008099f6314c0
psico <desenvolvedorjg@gmail.com>
2022-09-18 17:10:59 -0300

    [UPT] Format improved

--- src/page/Community/css.js
+++ src/page/Community/css.js
@@ -1,38 +1,38 @@
 import {
-    makeStyles
+  makeStyles
 } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: "flex",
-        flexDirection: "column",
-        flexGrow: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        width: "100%",
-        maxWidth: "921px",
-        margin: 0,
-        paddingTop: "115px",
-        paddingBottom: "50px",
-        fontFamily: "sans-serif",
-        background: "#dfdfdf",
-    },
-    paper: {
-        padding: theme.spacing(1),
-        marginBottom: "15px",
-        textAlign: 'center'
-    },
-    item: {
-        paddingBottom: "10px"
-    },
-    vLine: {
-        borderRight: "1px solid #eee",
-        padding: "5px"
-    },
-    hLine: {
-        borderBottom: "1px solid #eee",
-        padding: "5px"
-    }
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    maxWidth: '921px',
+    margin: 0,
+    paddingTop: '115px',
+    paddingBottom: '50px',
+    fontFamily: 'sans-serif',
+    background: '#dfdfdf',
+  },
+  paper: {
+    padding: theme.spacing(1),
+    marginBottom: '15px',
+    textAlign: 'center'
+  },
+  item: {
+    paddingBottom: '10px'
+  },
+  vLine: {
+    borderRight: '1px solid #eee',
+    padding: '5px'
+  },
+  hLine: {
+    borderBottom: '1px solid #eee',
+    padding: '5px'
+  }
 }));
 
 export default useStyles;

819f8e0efc97253dbaa686dc44eb37b5a81df9fc
psico <desenvolvedorjg@gmail.com>
2022-09-20 19:28:13 -0300

    [UPT] Improving code

--- src/components/Match/css.js
+++ src/components/Match/css.js
@@ -4,34 +4,34 @@ import {
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex",
-    flexDirection: "column",
+    display: 'flex',
+    flexDirection: 'column',
     flexGrow: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    maxWidth: "921px",
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    maxWidth: '921px',
     margin: 0,
-    paddingTop: "115px",
-    paddingBottom: "50px",
-    fontFamily: "sans-serif",
-    background: "#dfdfdf",
+    paddingTop: '115px',
+    paddingBottom: '50px',
+    fontFamily: 'sans-serif',
+    background: '#dfdfdf',
   },
   paper: {
     padding: theme.spacing(1),
-    marginBottom: "15px",
+    marginBottom: '15px',
     textAlign: 'center'
   },
   item: {
-    paddingBottom: "10px"
+    paddingBottom: '10px'
   },
   vLine: {
-    borderRight: "1px solid #eee",
-    padding: "5px"
+    borderRight: '1px solid #eee',
+    padding: '5px'
   },
   hLine: {
-    borderBottom: "1px solid #eee",
-    padding: "5px"
+    borderBottom: '1px solid #eee',
+    padding: '5px'
   }
 }));
 

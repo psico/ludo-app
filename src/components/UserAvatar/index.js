624d6ce37c37509794800ecef51cadff31e33562
psico <desenvolvedorjg@gmail.com>
2022-09-27 16:56:56 -0300

    [UPT] Improving code

--- src/components/UserAvatar/index.js
+++ src/components/UserAvatar/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import PersonAvatar from "../PersonAvatar";
 
 const UserAvatar = ({ displayName, photoURL, showName }) => {
-    console.log("showName ==> ", showName, showName || false)
     return (
         <PersonAvatar
             displayName={displayName || 'Unknown'}

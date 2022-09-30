b1099e369eb1ffe1943a9c484716c208309a6ea2
psico <desenvolvedorjg@gmail.com>
2022-09-29 21:42:48 -0300

    [FIX] Photo profile

--- src/components/UserAvatar/index.js
+++ src/components/UserAvatar/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import PersonAvatar from "../PersonAvatar";
 
-const UserAvatar = ({ displayName, photoURL, showName }) => {
+const UserAvatar = ({ displayName, photoURL, showName, photoSize }) => {
     return (
         <PersonAvatar
             displayName={displayName || 'Unknown'}
             photoURL={photoURL || 'Unknown'}
             showName={!!showName}
+            photoSize={photoSize}
         />
     )
 };

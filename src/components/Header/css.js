bf35ea08df0e2fe9e7206816bf8640a90c9b5c75
psico <desenvolvedorjg@gmail.com>
2022-10-01 16:46:58 -0300

    [UPT] Removed descrepted color

--- src/components/Header/css.js
+++ src/components/Header/css.js
@@ -1,5 +1,5 @@
 import {
-    fade,
+    alpha,
     makeStyles
 } from '@material-ui/core';
 
@@ -19,9 +19,9 @@ const componentStyles = makeStyles(theme => ({
     search: {
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
+        backgroundColor: alpha(theme.palette.common.white, 0.15),
         '&:hover': {
-            backgroundColor: fade(theme.palette.common.white, 0.25),
+            backgroundColor: alpha(theme.palette.common.white, 0.25),
         },
         marginRight: theme.spacing(2),
         marginLeft: 0,

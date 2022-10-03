a8681e79aa9150938af8099a97f779ccbf829853
psico <desenvolvedorjg@gmail.com>
2022-10-03 10:24:23 -0300

    [UPT] Improving CSS code

--- src/components/Header/css.js
+++ src/components/Header/css.js
@@ -46,12 +46,12 @@ const componentStyles = makeStyles(theme => ({
     inputInput: {
         padding: theme.spacing(1, 1, 1, 0),
         // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
+        // paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        // transition: theme.transitions.create('width'),
+        // width: '100%',
+        // [theme.breakpoints.up('md')]: {
+        //     width: '20ch',
+        // },
     },
     positionHeader: {
         // display: "flex",

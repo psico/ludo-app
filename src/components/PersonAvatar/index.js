70fa799e9e1a361d8b96b8325a71998f5529ba51
psico <desenvolvedorjg@gmail.com>
2022-10-05 18:23:31 -0300

    [UPT] Improving code

--- src/components/PersonAvatar/index.js
+++ src/components/PersonAvatar/index.js
@@ -13,8 +13,8 @@ const PersonAvatar = ({
 
     return (
         <div className={classes.root}>
-            <Avatar variant="rounded" alt={displayName} src={photoURL} className={photoSize}>
-                {displayName.substr(0,1)}
+            <Avatar variant="rounded" alt={displayName} src={require(photoURL)} className={photoSize}>
+                {displayName.substring(0,1)}
             </Avatar>
             {showName === true && <div className={classes.textAvatar}>{displayName}</div>}
         </div>

ff5895897233baad80cd8ff92bd433514a7dc417
psico <desenvolvedorjg@gmail.com>
2022-11-08 21:50:51 -0300

    [UPT] Better display

--- src/components/MatchesSearch/index.js
+++ src/components/MatchesSearch/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useStyles from '../ProfileInfo/css';
+import useStyles from './css';
 import Paper from '@material-ui/core/Paper';
 import { useParams, withRouter } from 'react-router-dom/cjs/react-router-dom';
 import Grid from '@material-ui/core/Grid';
